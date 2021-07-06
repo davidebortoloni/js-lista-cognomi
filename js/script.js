ad15f43ec0fe25fa4819bec1010674fc7648d073
davidebortoloni <davidebortoloni@gmail.com>
2021-07-06 16:21:27 +0200

    Ottimizzazione codice [esercizio finito]

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
+// Creazione variabili
 var surnamesID = document.getElementById("surnames");
 var positionID = document.getElementById("position");
 var surnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
-
-var userSurname = capitalize(prompt("Inserisci il tuo cognome").trim());
-while (!userSurname || !allLetter(userSurname)) {
-    alert("Formato non valido");
-    var userSurname = capitalize(prompt("Inserisci il tuo cognome").trim());
-}
-surnames.push(userSurname);
-surnames.sort()
-
 var surnamesList = "";
 var position;
+
+// Input cognome
+var userSurname = prompt("Inserisci il tuo cognome");
+while (!userSurname || !allLetter(userSurname)) {
+    alert("Formato non valido");
+    var userSurname = prompt("Inserisci il tuo cognome");
+}
+
+// Inserimento cognome
+userSurname = capitalize(userSurname.trim());
+surnames.push(userSurname);
+
+// Ordinamento lista
+surnames.sort()
+
+// Preparazione alla stampa
 for (var i = 0; i < surnames.length; i++) {
     if (surnames[i] == userSurname) {
         position = i + 1;
     }
     surnamesList += "<li>" + surnames[i] + "</li>";
 }
-console.table(surnames);
-surnamesID.innerHTML = surnamesList;
 positionMessage = "Il tuo cognome è in " + position + "° posizione";
+
+// Stampa in console e in pagina
+console.table(surnames);
 console.log(positionMessage);
+surnamesID.innerHTML = surnamesList;
 positionID.innerHTML = positionMessage;
 
+// Metodi
 function allLetter(inputTxt) {
     var letters = /^[a-zA-Z\s]+$/;
     if (inputTxt.match(letters)) {
